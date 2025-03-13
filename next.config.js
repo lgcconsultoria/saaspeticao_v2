1ec23604341a182110633f6742e1b4d89edcb744
LGC Consultoria <lgcconsultoria93@gmail.com>
2025-03-13 20:02:32 -0300

    Adicionar configuração do Next.js

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
